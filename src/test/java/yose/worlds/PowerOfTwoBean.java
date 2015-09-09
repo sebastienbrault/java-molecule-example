1999ee2adfd1eb338a599d2dbc368cb808660f85
equipeenavant <maite.cartigny@orange.com>
2015-09-09 16:20:27 +0200

    Converti en JSON et création de la fonction powerOfTwoController

---
+++ src/test/java/yose/worlds/PowerOfTwoBean.java
@@ -0,0 +1,35 @@
+package yose.worlds;
+
+/**
+ * Bean pour la transformation en Gson pour le world PowerOfTwo
+ * 
+ * @author XHLX8486
+ *
+ */
+public class PowerOfTwoBean {
+	private Integer number;
+	private Integer[] decomposition;
+
+	public PowerOfTwoBean(Integer number, Integer[] decomposition) {
+		super();
+		this.number = number;
+		this.decomposition = decomposition;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public Integer[] getDecomposition() {
+		return decomposition;
+	}
+
+	public void setDecomposition(Integer[] decomposition) {
+		this.decomposition = decomposition;
+	}
+
+}
